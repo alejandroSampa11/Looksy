f41b1a5e1f10664ad23d87022933721f2fb8ee34
pacho ruiz <alfrrugu@hotmail.com>
2025-05-20 15:13:49 -0600

    Mongo config (#6)
    * Test main
    
    * MONGO-CONFIG connection check for .env

--- backend-looksy/src/app.ts
+++ backend-looksy/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import { connectDB } from "./config/db";
+
 
 const app = express();
 
@@ -8,5 +10,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes);
+
+const PORT = process.env.PORT || 1000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error: any) {
+    console.error("Failed to start server", error);
+    process.exit(1);
+  }
+};
+
+startServer()
+
 
 export default app;
